1d8208bc45b51bb692a8a8cc0ba32692b7dc1281
李俊杰 <1061455426@qq.com>
2021-08-06 00:57:26 +0800

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void vmprint(pagetable_t pagetable,uint64 depth){
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf("..");
+      int d=depth;
+      while(d>1){
+        printf(" ..");
+        --d;
+      }
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      printf("%d: pte %p pa %p\n",i,pte,child);
+      //没有设置过flag表示不是最低一层，递归
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        vmprint((pagetable_t)child,depth+1);
+      }
+    } 
+    // else if(pte & PTE_V){
+    //   panic("freewalk: leaf");
+    // }
+  }
+}
