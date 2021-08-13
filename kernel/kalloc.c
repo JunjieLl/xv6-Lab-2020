a07617a3572f36bfb79f29d6eb9445990f63bde8
李俊杰 <1061455426@qq.com>
2021-08-14 03:53:48 +0800

    内存分配器 锁

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,15 +18,21 @@ struct run {
   struct run *next;
 };
 
+#define LOCKNAMEMAXLEN 20
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  char lockName[LOCKNAMEMAXLEN];
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i=0;i<NCPU;++i){
+    snprintf(kmem[i].lockName, LOCKNAMEMAXLEN,"kmem%d",i);
+    initlock(&kmem[i].lock,kmem[i].lockName);
+  }
+  //initlock(&kmem.lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -46,6 +52,9 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+  push_off();
+  int cpuId=cpuid();
+
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -56,10 +65,12 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[cpuId].lock);
+  r->next = kmem[cpuId].freelist;
+  kmem[cpuId].freelist = r;
+  release(&kmem[cpuId].lock);
+
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,15 +79,42 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
+  push_off();
+  int cpuId=cpuid();
+
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  //acquire(&kmem.lock);
+  // r = kmem.freelist;
+  // if(r)
+  //   kmem.freelist = r->next;
+  //release(&kmem.lock);
+  acquire(&kmem[cpuId].lock);
+  r=kmem[cpuId].freelist;
+  if(r){
+    kmem[cpuId].freelist=r->next;
+    release(&kmem[cpuId].lock);
+  }
+  else{
+    release(&kmem[cpuId].lock);
+    for(int otherCPU=0;otherCPU<NCPU;++otherCPU){
+      if(otherCPU!=cpuId){
+        acquire(&kmem[otherCPU].lock);
+        r=kmem[otherCPU].freelist;
+        if(r){
+          kmem[otherCPU].freelist=r->next;
+          release(&kmem[otherCPU].lock);
+          break;
+        }
+        release(&kmem[otherCPU].lock);
+      }
+    }
+  }
+
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+
   return (void*)r;
 }
